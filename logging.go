1c9870001583fa3161c484378c9f2b332f4901bb
Christian Muehlhaeuser <muesli@gmail.com>
2020-01-25 07:15:56 +0100

    syslog is only available on unix systems

---
+++ logging.go
@@ -0,0 +1,22 @@
+// +build darwin dragonfly freebsd linux netbsd openbsd solaris
+
+package tea
+
+import (
+	"log"
+	"log/syslog"
+)
+
+// UseSysLog sets up logging to log the system log. This becomes helpful when
+// debugging since we can't easily print to the terminal since our TUI is
+// occupying it!
+//
+// On macOS this is a just a matter of: tail -f /var/log/system.log
+func UseSysLog(programName string) error {
+	l, err := syslog.New(syslog.LOG_NOTICE, programName)
+	if err != nil {
+		return err
+	}
+	log.SetOutput(l)
+	return nil
+}
--- tea.go
+++ tea.go
@@ -3,8 +3,7 @@ package tea
 import (
 	"errors"
 	"io"
-	"log"
-	"log/syslog"
+	"os"
 	"strings"
 )
 
@@ -175,18 +174,4 @@ func (p *Program) render(model Model, linesRendered int) int {
 	}
 	io.WriteString(os.Stdout, view)
 	return strings.Count(view, "\r\n")
-}
-
-// UseSysLog sets up logging to log the system log. This becomes helpful when
-// debugging since we can't easily print to the terminal since our TUI is
-// occupying it!
-//
-// On macOS this is a just a matter of: tail -f /var/log/system.log
-func UseSysLog(programName string) error {
-	l, err := syslog.New(syslog.LOG_NOTICE, programName)
-	if err != nil {
-		return err
-	}
-	log.SetOutput(l)
-	return nil
 }
