e089f652f603b8e6004eca345bb650494e126e1f
Shivan Sivakumaran <51132467+shivan-s@users.noreply.github.com>
2022-06-23 11:59:19 +1200

    remove title.py

--- examples/title.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-p = Path('.')
-dirs = [x for x in p.iterdir() if x.is_dir()]
-dirs.sort()
-
-for dir in dirs:
-    folder_name = " ".join([name.title() for name in str(dir).split("-")])
-    print(f"## [{folder_name}](/examples/{str(dir)})\n\n")
