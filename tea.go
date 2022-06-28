db4f6474c9c27e41742f3eb2b9951c37fd16da98
Maas Lalani <maas@lalani.dev>
2022-06-28 15:34:22 -0400

    doc: Fix `Printf` typo (#358)

--- tea.go
+++ tea.go
@@ -710,11 +710,8 @@ func (p *Program) RestoreTerminal() error {
 	return nil
 }
 
-// Printf prints above the Program. This output is unmanaged by the program and
-// will persist across renders by the Program.
-//
-// Unlike fmt.Printf (but similar to log.Printf) the message will be print on
-// its own line.
+// Println prints above the Program. This output is unmanaged by the program
+// and will persist across renders by the Program.
 //
 // If the altscreen is active no output will be printed.
 func (p *Program) Println(args ...interface{}) {
