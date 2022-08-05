07b18e5fd48ab7788e5c7dd6d487c3a70cce6335
bashbunni <bashbunni.io@gmail.com>
2022-08-05 09:01:04 -0700

    docs: add working nested model example

--- examples/nested-models/main.go
+++ examples/nested-models/main.go
@@ -1,47 +1,145 @@
 package main
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"log"
+	"time"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/timer"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
 
 // this is an enum for Go
 type sessionState uint
 
 const (
-	timerView sessionState = iota
-	statsView
+	defaultTime              = time.Minute
+	timerView   sessionState = iota
+	spinnerView
+)
+
+var (
+	// Available spinners
+	spinners = []spinner.Spinner{
+		spinner.Line,
+		spinner.Dot,
+		spinner.MiniDot,
+		spinner.Jump,
+		spinner.Pulse,
+		spinner.Points,
+		spinner.Globe,
+		spinner.Moon,
+		spinner.Monkey,
+	}
+	spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("69"))
+	helpStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
 )
 
 type mainModel struct {
-	state sessionState
+	state   sessionState
+	timer   timer.Model
+	spinner spinner.Model
+	index   int
 }
 
-func New() mainModel {
+// New: Create a new main model
+func New(timeout time.Duration) mainModel {
 	// initialize your model; timerView is the first "view" we want to see
-	return mainModel{state: timerView}
+	m := mainModel{state: timerView}
+	m.timer = timer.New(timeout)
+	m.spinner = spinner.New()
+	return m
 }
 
 func (m mainModel) Init() tea.Cmd {
-	return nil
+	// start the timer and spinner on program start
+	return tea.Batch(m.timer.Init(), m.spinner.Tick)
 }
 
 func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	// Handle IO -> keypress, WindowSizeMSg
 	case tea.KeyMsg:
-		if msg.String() == "ctrl+c" || msg.String() == "q" {
+		switch msg.String() {
+		case "ctrl+c", "q":
 			return m, tea.Quit
+		case "enter":
+			if m.state == timerView {
+				m.state = spinnerView
+			} else {
+				m.state = timerView
+			}
+			return m, nil
+		case "n":
+			var cmd tea.Cmd
+			if m.state == timerView {
+				m.timer = timer.New(defaultTime)
+				cmd = m.timer.Init()
+			} else {
+				m.Next()
+				m.resetSpinner()
+				cmd = spinner.Tick
+			}
+			return m, cmd
 		}
-	case tea.WindowSizeMsg:
-		// handle resizing windows
-		// handle your Msgs
+		switch m.state {
+		// update whichever model is focused
+		case spinnerView:
+			var cmd tea.Cmd
+			m.spinner, cmd = m.spinner.Update(msg)
+			return m, cmd
+		default:
+			// another way to do it
+			newModel, cmd := m.timer.Update(msg)
+			m.timer = newModel
+			// if this were your own model, you would need to wrap the type
+			// before assignment because its type would be tea.Model
+			// i.e. m.list = newModel.(list.Model)
+			return m, cmd
+		}
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	case timer.TickMsg:
+		var cmd tea.Cmd
+		m.timer, cmd = m.timer.Update(msg)
+		return m, cmd
 	}
 	return m, nil
 }
 
 func (m mainModel) View() string {
+	var s string
 	switch m.state {
-	case statsView:
-		return statsView.View()
+	case spinnerView:
+		s += m.spinner.View() + "\n"
 	default:
-		return "timer is " + timerView.View()
+		s += m.timer.View() + "\n"
+	}
+	s += helpStyle("enter: change view • n: new spinner/timer • q: exit\n")
+	return s
+}
+
+func (m *mainModel) Next() {
+	if m.index == len(spinners)-1 {
+		m.index = 0
+	} else {
+		m.index++
+	}
+}
+
+func (m *mainModel) resetSpinner() {
+	m.spinner = spinner.New()
+	m.spinner.Style = spinnerStyle
+	m.spinner.Spinner = spinners[m.index]
+}
+
+func main() {
+	p := tea.NewProgram(New(defaultTime))
+
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
 	}
 }
