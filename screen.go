00370a290c3616771071f03cd2cd63acc2ef25ad
Raphael 'kena' Poss <knz@thaumogen.net>
2022-08-25 19:13:30 +0200

    feat: support a Clear command

--- screen.go
+++ screen.go
@@ -9,6 +9,20 @@ type WindowSizeMsg struct {
 	Height int
 }
 
+// ClearScreen is a special command that tells the program to clear the screen
+// before the next update. This can be used to move the cursor to the top left
+// of the screen and clear visual clutter when the alt screen is not in use.
+//
+// Note that it should never be necessary to call ClearScreen() for regular
+// redraws.
+func ClearScreen() Msg {
+	return clearScreenMsg{}
+}
+
+// clearScreenMsg is an internal message that signals to clear the screen.
+// You can send a clearScreenMsg with ClearScreen.
+type clearScreenMsg struct{}
+
 // EnterAltScreen is a special command that tells the Bubble Tea program to
 // enter the alternate screen buffer.
 //
