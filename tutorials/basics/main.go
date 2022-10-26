9c6bde3bda7a7445ed1a2799e648998c39c7981f
Patrick Coxall <mr.coxall@mths.ca>
2022-10-26 09:12:03 -0400

    I just worked through the tutorial and it will not work. I had to change "p.Run" with p.Start and note that it only returns 1 value and not 2.

--- tutorials/basics/main.go
+++ tutorials/basics/main.go
@@ -79,7 +79,7 @@ func (m model) View() string {
 
 func main() {
 	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
+	if err := p.Start(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
