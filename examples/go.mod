cee3c21de5e6ac6ca308952a60261d6211d7a21c
Tomtom <tomtom@herbesfolles.org>
2022-11-09 18:29:52 +0100

    first POC

--- examples/go.mod
+++ examples/go.mod
@@ -4,10 +4,12 @@ go 1.16
 
 require (
 	github.com/charmbracelet/bubbles v0.13.1-0.20220815142520-649f78e1fd8b
-	github.com/charmbracelet/bubbletea v0.22.0
+	github.com/charmbracelet/bubbletea v0.22.1
 	github.com/charmbracelet/glamour v0.5.0
 	github.com/charmbracelet/lipgloss v0.6.0
+	github.com/charmbracelet/wish v0.6.0
 	github.com/fogleman/ease v0.0.0-20170301025033-8da417bf1776
+	github.com/gliderlabs/ssh v0.3.5
 	github.com/lucasb-eyer/go-colorful v1.2.0
 	github.com/mattn/go-isatty v0.0.16
 	github.com/muesli/reflow v0.3.0
---
+++ examples/multichat/main.go
@@ -0,0 +1,197 @@
+package main
+
+// A simple program demonstrating the text area component from the Bubbles
+// component library.
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/wish"
+	bm "github.com/charmbracelet/wish/bubbletea"
+	lm "github.com/charmbracelet/wish/logging"
+	"github.com/gliderlabs/ssh"
+	"github.com/muesli/termenv"
+)
+
+const (
+	host = "localhost"
+	port = 23234
+)
+
+// App contient le serveur wish et l'état partagé
+type App struct {
+	*ssh.Server
+	Programs []*tea.Program
+}
+
+// Send envoit un tea.Msg a tous les clients du serveur wish
+func (a *App) Send(msg tea.Msg) {
+	for _, p := range a.Programs {
+		go p.Send(msg) // dans une goroutine pour éviter un deadlock
+	}
+	fmt.Println(msg)
+}
+
+func NewApp() *App {
+	a := new(App)
+
+	s, err := wish.NewServer(
+		wish.WithAddress(fmt.Sprintf("%s:%d", host, port)),
+		wish.WithHostKeyPath(".ssh/term_info_ed25519"),
+		wish.WithMiddleware(
+			bm.MiddlewareWithProgramHandler(a.ProgramHandler, termenv.ANSI256),
+			lm.Middleware(),
+		),
+	)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	a.Server = s
+	return a
+}
+
+func (a *App) Start() {
+	var err error
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Printf("Starting SSH server on %s:%d", host, port)
+	go func() {
+		if err = a.ListenAndServe(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	<-done
+	log.Println("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := a.Shutdown(ctx); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func (a *App) ProgramHandler(s ssh.Session) *tea.Program {
+	// vérifier la validité de la session
+	_, _, active := s.Pty()
+	if !active {
+		wish.Fatalln(s, "terminal inactif")
+	}
+
+	model := initialModel()
+	model.App = a
+
+	p := tea.NewProgram(model, tea.WithOutput(s), tea.WithInput(s))
+	a.Programs = append(a.Programs, p)
+
+	return p
+}
+
+func main() {
+	app := NewApp()
+	app.Start()
+}
+
+type chatMsg string
+
+type (
+	errMsg error
+)
+
+type model struct {
+	*App
+	viewport    viewport.Model
+	messages    []string
+	textarea    textarea.Model
+	senderStyle lipgloss.Style
+	err         error
+}
+
+func initialModel() model {
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+
+	ta.Prompt = "┃ "
+	ta.CharLimit = 280
+
+	ta.SetWidth(30)
+	ta.SetHeight(3)
+
+	// Remove cursor line styling
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+
+	ta.ShowLineNumbers = false
+
+	vp := viewport.New(30, 5)
+	vp.SetContent(`Welcome to the chat room!
+Type a message and press Enter to send.`)
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		textarea:    ta,
+		messages:    []string{},
+		viewport:    vp,
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			fmt.Println(m.textarea.Value())
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.App.Send(chatMsg(m.textarea.Value()))
+			m.textarea.Reset()
+		}
+
+	case chatMsg:
+		m.messages = append(m.messages, string(msg))
+		m.viewport.SetContent(strings.Join(m.messages, "\n"))
+		m.viewport.GotoBottom()
+
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s\n\n%s",
+		m.viewport.View(),
+		m.textarea.View(),
+	) + "\n\n"
+}
