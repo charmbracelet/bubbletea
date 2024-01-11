8f3464a75600e991bbce22229d6e5b99975416f0
Sharun <715417+sharunkumar@users.noreply.github.com>
2024-01-11 07:38:58 -0800

    handle "q" for quitting in list-simple example (#839)

--- examples/list-simple/main.go
+++ examples/list-simple/main.go
@@ -67,7 +67,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tea.KeyMsg:
 		switch keypress := msg.String(); keypress {
-		case "ctrl+c":
+		case "q", "ctrl+c":
 			m.quitting = true
 			return m, tea.Quit
 
