charmbracelet/bubbletea

commit 7d708384a105005dfbcec2290bfe4ea1d0e8d9f0

feat: add a cmd to request window size (#988)
* feat: add a cmd to request window size

Fixes: https://github.com/charmbracelet/bubbletea/issues/987#issuecomment-2068452658

* Update commands.go

Co-authored-by: Christian Rocha <christian@rocha.is>

---------

Co-authored-by: Christian Rocha <christian@rocha.is>

tea.go
@@ -422,6 +422,9 @@ func (p *Program) eventLoop(model Model, cmds chan Cmd) (Model, error) {
 
 			case setWindowTitleMsg:
 				p.SetWindowTitle(string(msg))
+
+			case windowSizeMsg:
+				p.checkResize()
 			}
 
 			// Process internal messages for the renderer.

commands.go
@@ -201,3 +201,16 @@ func SetWindowTitle(title string) Cmd {
 		return setWindowTitleMsg(title)
 	}
 }
+
+type windowSizeMsg struct{}
+
+// WindowSize is a command that queries the terminal for its current size. It
+// delivers the results to Update via a [WindowSizeMsg]. Keep in mind that
+// WindowSizeMsgs will automatically be delivered to Update when the [Program]
+// starts and when the window dimensions change so in many cases you will not
+// need to explicitly invoke this command.
+func WindowSize() Cmd {
+	return func() Msg {
+		return windowSizeMsg{}
+	}
+}

commit fa0c116d82d711b50144f4f97824c24c59156d31

fix: query window-size in a goroutine
We need to run checkResize in a goroutine, otherwise, it will block.

Related: https://github.com/charmbracelet/bubbletea/pull/988#issuecomment-2231811332
Fixes: 7d708384a105 (feat: add a cmd to request window size (#988))

examples/window-size/main.go
@@ -1,0 +1,44 @@
+package main
+
+// A simple program that queries and displays the window-size.
+
+import (
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	p := tea.NewProgram(model{})
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type model struct{}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if s := msg.String(); s == "ctrl+c" || s == "q" || s == "esc" {
+			return m, tea.Quit
+		}
+
+		return m, tea.WindowSize()
+
+	case tea.WindowSizeMsg:
+		return m, tea.Printf("%dx%d", msg.Width, msg.Height)
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "When you're done press q to quit. Press any other key to query the window-size.\n"
+
+	return s
+}

tea.go
@@ -424,7 +424,7 @@ func (p *Program) eventLoop(model Model, cmds chan Cmd) (Model, error) {
 				p.SetWindowTitle(string(msg))
 
 			case windowSizeMsg:
-				p.checkResize()
+				go p.checkResize()
 			}
 
 			// Process internal messages for the renderer.