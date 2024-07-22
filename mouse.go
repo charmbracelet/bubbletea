cca6eae2a28b9d5182d27e09905553e9f0b13d85
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-07-22 14:49:12 -0400

    feat: export mouse button consts

--- mouse.go
+++ mouse.go
@@ -49,3 +49,35 @@ func (m MouseWheelMsg) String() string {
 func (m MouseMotionMsg) String() string {
 	return input.MouseMotionEvent(m).String()
 }
+
+// Mouse event buttons
+//
+// This is based on X11 mouse button codes.
+//
+//	1 = left button
+//	2 = middle button (pressing the scroll wheel)
+//	3 = right button
+//	4 = turn scroll wheel up
+//	5 = turn scroll wheel down
+//	6 = push scroll wheel left
+//	7 = push scroll wheel right
+//	8 = 4th button (aka browser backward button)
+//	9 = 5th button (aka browser forward button)
+//	10
+//	11
+//
+// Other buttons are not supported.
+const (
+	MouseNone       = input.MouseNone
+	MouseLeft       = input.MouseLeft
+	MouseMiddle     = input.MouseMiddle
+	MouseRight      = input.MouseRight
+	MouseWheelUp    = input.MouseWheelUp
+	MouseWheelDown  = input.MouseWheelDown
+	MouseWheelLeft  = input.MouseWheelLeft
+	MouseWheelRight = input.MouseWheelRight
+	MouseBackward   = input.MouseBackward
+	MouseForward    = input.MouseForward
+	MouseExtra1     = input.MouseExtra1
+	MouseExtra2     = input.MouseExtra2
+)
