db374e331f625737d811727272a83163b66361f3
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-08-12 16:15:10 -0300

    docs: focus/blur example
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

---
+++ examples/focus-blur/main.go
@@ -0,0 +1,52 @@
+package main
+
+// A simple program that handled losing and acquiring focus.
+
+import (
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	p := tea.NewProgram(model{}, tea.WithReportFocus())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type model struct {
+	focused bool
+}
+
+func (m model) Init() (tea.Model, tea.Cmd) {
+	m.focused = true
+	return m, nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.FocusMsg:
+		m.focused = true
+	case tea.BlurMsg:
+		m.focused = false
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "ctrl+z":
+			return m, tea.Suspend
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Hi. "
+	if m.focused {
+		s += "This program is currently focused!"
+	} else {
+		s += "This program is currently blurred!"
+	}
+	return s + "\n\nTo quit sooner press ctrl-c, or press ctrl-z to suspend...\n"
+}
