516b7cd4dcd0550479d85975e80b4502d171c2c6
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-09-12 11:49:22 -0400

    fix: renderer: nilRenderer doesn't need to be exported

--- nil_renderer.go
+++ nil_renderer.go
@@ -1,22 +1,22 @@
 package tea
 
-// NilRenderer is a no-op renderer. It implements the Renderer interface but
+// nilRenderer is a no-op renderer. It implements the Renderer interface but
 // doesn't render anything to the terminal.
-type NilRenderer struct{}
+type nilRenderer struct{}
 
-var _ renderer = NilRenderer{}
+var _ renderer = nilRenderer{}
 
 // flush implements the Renderer interface.
-func (NilRenderer) flush() error { return nil }
+func (nilRenderer) flush() error { return nil }
 
 // close implements the Renderer interface.
-func (NilRenderer) close() error { return nil }
+func (nilRenderer) close() error { return nil }
 
 // render implements the Renderer interface.
-func (NilRenderer) render(string) {}
+func (nilRenderer) render(string) {}
 
 // reset implements the Renderer interface.
-func (NilRenderer) reset() {}
+func (nilRenderer) reset() {}
 
 // update implements the Renderer interface.
-func (NilRenderer) update(Msg) {}
+func (nilRenderer) update(Msg) {}
