charmbracelet/bubbletea

commit 7cb34becafd6eb1716a8eb4251da44fded6376ea

(v2) feat: combine keyboard enhancements into a nicer API (#1152)
* feat: combine keyboard enhancements into a nicer API

This change combines the keyboard enhancements into a nicer API. Kitty
keyboard protocol and XTerm modifyOtherKeys are now combined into a
single API. This makes it easier to enable keyboard enhancements.

Use `WithKeyboardEnhancements` to enable keyboard enhancements. This
function accepts a list of `KeyboardEnhancement` functions that can be
used to enable different keyboard features.

For now, we only support the `WithReleaseKeys` enhancement which enables
support for reporting release key events.

* Update options.go

Co-authored-by: Christian Rocha <christian@rocha.is>

* chore: go mod tidy

* chore: gofumpt

* fix: simplify keyboard enhancements disambiguation

* feat: make KeyboardEnhancementsMsg helpers always return true on Windows

* fix: remove unused win32input mode

* refactor: rename KeyboardEnhancementsMsg methods

---------

Co-authored-by: Christian Rocha <christian@rocha.is>

tea.go
@@ -101,9 +101,7 @@ const (
 	withoutCatchPanics
 	withoutBracketedPaste
 	withReportFocus
-	withKittyKeyboard
-	withModifyOtherKeys
-	withWindowsInputMode
+	withKeyboardEnhancements
 	withoutGraphemeClustering
 )
 
@@ -204,11 +202,7 @@ type Program struct {
 	// rendererDone is used to stop the renderer.
 	rendererDone chan struct{}
 
-	// kittyFlags stores kitty keyboard protocol progressive enhancement flags.
-	kittyFlags int
-
-	// modifyOtherKeys stores the XTerm modifyOtherKeys mode.
-	modifyOtherKeys int
+	keyboard keyboardEnhancements
 
 	// When a program is suspended, the terminal state is saved and the program
 	// is paused. This saves the terminal colors state so they can be restored
@@ -475,34 +469,41 @@ func (p *Program) eventLoop(model Model, cmds chan Cmd) (Model, error) {
 			case cursorColorMsg:
 				p.execute(ansi.RequestCursorColor)
 
-			case KittyKeyboardMsg:
-				// Store the kitty flags whenever they are queried.
-				p.kittyFlags = int(msg)
-
-			case setKittyKeyboardFlagsMsg:
-				p.kittyFlags = int(msg)
-				p.execute(ansi.PushKittyKeyboard(p.kittyFlags))
-
-			case kittyKeyboardMsg:
-				p.execute(ansi.RequestKittyKeyboard)
-
-			case modifyOtherKeys:
-				p.execute(ansi.RequestModifyOtherKeys)
-
-			case setModifyOtherKeysMsg:
-				p.modifyOtherKeys = int(msg)
-				p.execute(ansi.ModifyOtherKeys(p.modifyOtherKeys))
-
-			case setEnhancedKeyboardMsg:
-				if bool(msg) {
-					p.kittyFlags = 3
-					p.modifyOtherKeys = 1
-				} else {
-					p.kittyFlags = 0
-					p.modifyOtherKeys = 0
+			case KeyboardEnhancementsMsg:
+				if msg.kittyFlags != p.keyboard.kittyFlags {
+					msg.kittyFlags |= p.keyboard.kittyFlags
 				}
-				p.execute(ansi.ModifyOtherKeys(p.modifyOtherKeys))
-				p.execute(ansi.PushKittyKeyboard(p.kittyFlags))
+				if msg.modifyOtherKeys == 0 {
+					msg.modifyOtherKeys = p.keyboard.modifyOtherKeys
+				}
+
+			case enableKeyboardEnhancementsMsg:
+				var ke keyboardEnhancements
+				for _, e := range msg {
+					e(&ke)
+				}
+
+				p.keyboard.kittyFlags |= ke.kittyFlags
+				if ke.modifyOtherKeys > p.keyboard.modifyOtherKeys {
+					p.keyboard.modifyOtherKeys = ke.modifyOtherKeys
+				}
+
+				if p.keyboard.modifyOtherKeys > 0 {
+					p.execute(ansi.ModifyOtherKeys(p.keyboard.modifyOtherKeys))
+				}
+				if p.keyboard.kittyFlags > 0 {
+					p.execute(ansi.PushKittyKeyboard(p.keyboard.kittyFlags))
+				}
+
+			case disableKeyboardEnhancementsMsg:
+				if p.keyboard.modifyOtherKeys > 0 {
+					p.execute(ansi.DisableModifyOtherKeys)
+					p.keyboard.modifyOtherKeys = 0
+				}
+				if p.keyboard.kittyFlags > 0 {
+					p.execute(ansi.DisableKittyKeyboard)
+					p.keyboard.kittyFlags = 0
+				}
 
 			case execMsg:
 				// NB: this blocks.
@@ -705,20 +706,20 @@ func (p *Program) Run() (Model, error) {
 		p.modes[ansi.MouseAllMotionMode] = true
 		p.modes[ansi.MouseSgrExtMode] = true
 	}
-	if p.startupOptions&withModifyOtherKeys != 0 {
-		p.execute(ansi.ModifyOtherKeys(p.modifyOtherKeys))
-	}
-	if p.startupOptions&withKittyKeyboard != 0 {
-		p.execute(ansi.PushKittyKeyboard(p.kittyFlags))
-	}
 
 	if p.startupOptions&withReportFocus != 0 {
 		p.execute(ansi.EnableReportFocus)
 		p.modes[ansi.ReportFocusMode] = true
 	}
-	if p.startupOptions&withWindowsInputMode != 0 {
-		p.execute(ansi.EnableWin32Input)
-		p.modes[ansi.Win32InputMode] = true
+	if p.startupOptions&withKeyboardEnhancements != 0 {
+		if p.keyboard.modifyOtherKeys > 0 {
+			p.execute(ansi.ModifyOtherKeys(p.keyboard.modifyOtherKeys))
+			p.execute(ansi.RequestModifyOtherKeys)
+		}
+		if p.keyboard.kittyFlags > 0 {
+			p.execute(ansi.PushKittyKeyboard(p.keyboard.kittyFlags))
+			p.execute(ansi.RequestKittyKeyboard)
+		}
 	}
 
 	// Start the renderer.
@@ -913,11 +914,11 @@ func (p *Program) RestoreTerminal() error {
 	if p.modes[ansi.BracketedPasteMode] {
 		p.execute(ansi.EnableBracketedPaste)
 	}
-	if p.modifyOtherKeys != 0 {
-		p.execute(ansi.ModifyOtherKeys(p.modifyOtherKeys))
+	if p.keyboard.modifyOtherKeys != 0 {
+		p.execute(ansi.ModifyOtherKeys(p.keyboard.modifyOtherKeys))
 	}
-	if p.kittyFlags != 0 {
-		p.execute(ansi.PushKittyKeyboard(p.kittyFlags))
+	if p.keyboard.kittyFlags != 0 {
+		p.execute(ansi.PushKittyKeyboard(p.keyboard.kittyFlags))
 	}
 	if p.modes[ansi.ReportFocusMode] {
 		p.execute(ansi.EnableReportFocus)

commands.go
@@ -214,25 +214,3 @@ func WindowSize() Cmd {
 		return windowSizeMsg{}
 	}
 }
-
-// setEnhancedKeyboardMsg is a message to enable/disable enhanced keyboard
-// features.
-type setEnhancedKeyboardMsg bool
-
-// EnableEnhancedKeyboard is a command to enable enhanced keyboard features.
-// This unambiguously reports more key combinations than traditional terminal
-// keyboard sequences. This might also enable reporting of release key events
-// depending on the terminal emulator supporting it.
-//
-// This is equivalent to calling EnablieKittyKeyboard(3) and
-// EnableModifyOtherKeys(1).
-func EnableEnhancedKeyboard() Msg {
-	return setEnhancedKeyboardMsg(true)
-}
-
-// DisableEnhancedKeyboard is a command to disable enhanced keyboard features.
-//
-// This is equivalent to calling DisableKittyKeyboard() and DisableModifyOtherKeys().
-func DisableEnhancedKeyboard() Msg {
-	return setEnhancedKeyboardMsg(false)
-}

examples/print-key/main.go
@@ -14,12 +14,17 @@ func (m model) Init() (tea.Model, tea.Cmd) {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.KeyPressMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			return m, tea.Quit
+	case tea.KeyboardEnhancementsMsg:
+		return m, tea.Printf("Keyboard enhancements enabled! ReleaseKeys: %v\n", msg.SupportsKeyReleases())
+	case tea.KeyMsg:
+		switch msg := msg.(type) {
+		case tea.KeyPressMsg:
+			switch msg.String() {
+			case "ctrl+c":
+				return m, tea.Quit
+			}
 		}
-		return m, tea.Printf("You pressed: %s\n", msg.String())
+		return m, tea.Printf("You pressed: %s (%T)\n", msg.String(), msg)
 	}
 	return m, nil
 }
@@ -29,7 +34,7 @@ func (m model) View() string {
 }
 
 func main() {
-	p := tea.NewProgram(model{}, tea.WithEnhancedKeyboard())
+	p := tea.NewProgram(model{}, tea.WithKeyboardEnhancements(tea.WithKeyReleases))
 	if _, err := p.Run(); err != nil {
 		log.Printf("Error running program: %v", err)
 	}

keyboard.go
@@ -1,0 +1,104 @@
+package tea
+
+import (
+	"runtime"
+
+	"github.com/charmbracelet/x/ansi"
+)
+
+// keyboardEnhancements is a type that represents a set of keyboard
+// enhancements.
+type keyboardEnhancements struct {
+	// Kitty progressive keyboard enhancements protocol. This can be used to
+	// enable different keyboard features.
+	//
+	//  - 0: disable all features
+	//  - 1: [ansi.DisambiguateEscapeCodes] Disambiguate escape codes such as
+	//  ctrl+i and tab, ctrl+[ and escape, ctrl+space and ctrl+@, etc.
+	//  - 2: [ansi.ReportEventTypes] Report event types such as key presses,
+	//  releases, and repeat events.
+	//  - 4: [ansi.ReportAlternateKeys] Report keypresses as though they were
+	//  on a PC-101 ANSI US keyboard layout regardless of what they layout
+	//  actually is. Also include information about whether or not is enabled,
+	//  - 8: [ansi.ReportAllKeysAsEscapeCodes] Report all key events as escape
+	//  codes. This includes simple printable keys like "a" and other Unicode
+	//  characters.
+	//  - 16: [ansi.ReportAssociatedText] Report associated text with key
+	//  events. This encodes multi-rune key events as escape codes instead of
+	//  individual runes.
+	//
+	kittyFlags int
+
+	// Xterm modifyOtherKeys feature.
+	//
+	//  - Mode 0 disables modifyOtherKeys.
+	//  - Mode 1 reports ambiguous keys as escape codes. This is similar to
+	//  [ansi.KittyDisambiguateEscapeCodes] but uses XTerm escape codes.
+	//  - Mode 2 reports all key as escape codes including printable keys like "a" and "shift+b".
+	modifyOtherKeys int
+}
+
+// KeyboardEnhancement is a type that represents a keyboard enhancement.
+type KeyboardEnhancement func(k *keyboardEnhancements)
+
+// WithKeyReleases enables support for reporting release key events. This is
+// useful for terminals that support the Kitty keyboard protocol "Report event
+// types" progressive enhancement feature.
+//
+// Note that not all terminals support this feature.
+func WithKeyReleases(k *keyboardEnhancements) {
+	k.kittyFlags |= ansi.KittyReportEventTypes
+}
+
+// withKeyDisambiguation enables support for disambiguating keyboard escape
+// codes. This is useful for terminals that support the Kitty keyboard protocol
+// "Disambiguate escape codes" progressive enhancement feature or the XTerm
+// modifyOtherKeys mode 1 feature to report ambiguous keys as escape codes.
+func withKeyDisambiguation(k *keyboardEnhancements) {
+	k.kittyFlags |= ansi.KittyDisambiguateEscapeCodes
+	if k.modifyOtherKeys < 1 {
+		k.modifyOtherKeys = 1
+	}
+}
+
+type enableKeyboardEnhancementsMsg []KeyboardEnhancement
+
+// EnableKeyboardEnhancements is a command that enables keyboard enhancements
+// in the terminal.
+func EnableKeyboardEnhancements(enhancements ...KeyboardEnhancement) Cmd {
+	return func() Msg {
+		return enableKeyboardEnhancementsMsg(append(enhancements, withKeyDisambiguation))
+	}
+}
+
+type disableKeyboardEnhancementsMsg struct{}
+
+// DisableKeyboardEnhancements is a command that disables keyboard enhancements
+// in the terminal.
+func DisableKeyboardEnhancements() Msg {
+	return disableKeyboardEnhancementsMsg{}
+}
+
+// KeyboardEnhancementsMsg is a message that gets sent when the terminal
+// supports keyboard enhancements.
+type KeyboardEnhancementsMsg keyboardEnhancements
+
+// SupportsKeyDisambiguation returns whether the terminal supports reporting
+// disambiguous keys as escape codes.
+func (k KeyboardEnhancementsMsg) SupportsKeyDisambiguation() bool {
+	if runtime.GOOS == "windows" {
+		// We use Windows Console API which supports reporting disambiguous keys.
+		return true
+	}
+	return k.kittyFlags&ansi.KittyDisambiguateEscapeCodes != 0 || k.modifyOtherKeys >= 1
+}
+
+// SupportsKeyReleases returns whether the terminal supports key release
+// events.
+func (k KeyboardEnhancementsMsg) SupportsKeyReleases() bool {
+	if runtime.GOOS == "windows" {
+		// We use Windows Console API which supports key release events.
+		return true
+	}
+	return k.kittyFlags&ansi.KittyReportEventTypes != 0
+}

kitty.go
@@ -7,59 +7,6 @@ import (
 	"github.com/charmbracelet/x/ansi"
 )
 
-// setKittyKeyboardFlagsMsg is a message to set Kitty keyboard progressive
-// enhancement protocol flags.
-type setKittyKeyboardFlagsMsg int
-
-// EnableKittyKeyboard is a command to enable Kitty keyboard progressive
-// enhancements.
-//
-// The flags parameter is a bitmask of the following
-//
-//	1:  Disambiguate escape codes
-//	2:  Report event types
-//	4:  Report alternate keys
-//	8:  Report all keys as escape codes
-//	16: Report associated text
-//
-// See https://sw.kovidgoyal.net/kitty/keyboard-protocol/ for more information.
-func EnableKittyKeyboard(flags int) Cmd { //nolint:unused
-	return func() Msg {
-		return setKittyKeyboardFlagsMsg(flags)
-	}
-}
-
-// DisableKittyKeyboard is a command to disable Kitty keyboard progressive
-// enhancements.
-func DisableKittyKeyboard() Msg { //nolint:unused
-	return setKittyKeyboardFlagsMsg(0)
-}
-
-// kittyKeyboardMsg is a message that queries the current Kitty keyboard
-// progressive enhancement flags.
-type kittyKeyboardMsg struct{}
-
-// KittyKeyboard is a command that queries the current Kitty keyboard
-// progressive enhancement flags from the terminal.
-func KittyKeyboard() Msg { //nolint:unused
-	return kittyKeyboardMsg{}
-}
-
-// KittyKeyboardMsg is a bitmask message representing Kitty keyboard
-// progressive enhancement flags.
-//
-// The bitmaps represents the following:
-//
-//	0:  Disable all features
-//	1:  Disambiguate escape codes
-//	2:  Report event types
-//	4:  Report alternate keys
-//	8:  Report all keys as escape codes
-//	16: Report associated text
-//
-// See https://sw.kovidgoyal.net/kitty/keyboard-protocol/ for more information.
-type KittyKeyboardMsg int
-
 // Kitty Clipboard Control Sequences
 var kittyKeyMap = map[int]rune{
 	ansi.BS:  KeyBackspace,

options.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"io"
 	"sync/atomic"
-
-	"github.com/charmbracelet/x/ansi"
 )
 
 // ProgramOption is used to set options when initializing a Program. Program can
@@ -253,72 +251,20 @@ func WithReportFocus() ProgramOption {
 	}
 }
 
-// WithEnhancedKeyboard enables support for enhanced keyboard features. This
-// unambiguously reports more key combinations than traditional terminal
-// keyboard sequences. This might also enable reporting of release key events
-// depending on the terminal emulator supporting it.
+// WithKeyboardEnhancements enables support for enhanced keyboard features. You
+// can enable different keyboard features by passing one or more
+// KeyboardEnhancement functions.
 //
-// This is a syntactic sugar for WithKittyKeyboard(7) and WithXtermModifyOtherKeys(1).
-func WithEnhancedKeyboard() ProgramOption {
-	return func(p *Program) {
-		_WithKittyKeyboard(ansi.KittyDisambiguateEscapeCodes |
-			ansi.KittyReportEventTypes |
-			ansi.KittyReportAlternateKeys,
-		)(p)
-		_WithModifyOtherKeys(1)(p)
+// This is not supported on all terminals. On Windows, these features are
+// enabled by default.
+func WithKeyboardEnhancements(enhancements ...KeyboardEnhancement) ProgramOption {
+	var ke keyboardEnhancements
+	for _, e := range append(enhancements, withKeyDisambiguation) {
+		e(&ke)
 	}
-}
-
-// _WithKittyKeyboard enables support for the Kitty keyboard protocol. This
-// protocol enables more key combinations and events than the traditional
-// ambiguous terminal keyboard sequences.
-//
-// Use flags to specify which features you want to enable.
-//
-//	0:  Disable all features
-//	1:  Disambiguate escape codes
-//	2:  Report event types
-//	4:  Report alternate keys
-//	8:  Report all keys as escape codes
-//	16: Report associated text
-//
-// See https://sw.kovidgoyal.net/kitty/keyboard-protocol/ for more information.
-func _WithKittyKeyboard(flags int) ProgramOption {
 	return func(p *Program) {
-		p.kittyFlags = flags
-		p.startupOptions |= withKittyKeyboard
-	}
-}
-
-// _WithModifyOtherKeys enables support for the XTerm modifyOtherKeys feature.
-// This feature allows the terminal to report ambiguous keys as escape codes.
-// This is useful for terminals that don't support the Kitty keyboard protocol.
-//
-// The mode can be one of the following:
-//
-//	0: Disable modifyOtherKeys
-//	1: Report ambiguous keys as escape codes
-//	2: Report ambiguous keys as escape codes including modified keys like Alt-<key>
-//	   and Meta-<key>
-//
-// See https://invisible-island.net/xterm/manpage/xterm.html#VT100-Widget-Resources:modifyOtherKeys
-func _WithModifyOtherKeys(mode int) ProgramOption {
-	return func(p *Program) {
-		p.modifyOtherKeys = mode
-		p.startupOptions |= withModifyOtherKeys
-	}
-}
-
-// _WithWindowsInputMode enables Windows Input Mode (win32-input-mode) which
-// allows for more advanced input handling and reporting. This is experimental
-// and may not work on all terminals.
-//
-// See
-// https://github.com/microsoft/terminal/blob/main/doc/specs/%234999%20-%20Improved%20keyboard%20handling%20in%20Conpty.md
-// for more information.
-func _WithWindowsInputMode() ProgramOption { //nolint:unused
-	return func(p *Program) {
-		p.startupOptions |= withWindowsInputMode
+		p.startupOptions |= withKeyboardEnhancements
+		p.keyboard = ke
 	}
 }
 

parse.go
@@ -250,7 +250,7 @@ func parseCsi(b []byte) (int, Msg) {
 	case 'u' | '?'<<parser.MarkerShift:
 		// Kitty keyboard flags
 		if param := csi.Param(0); param != -1 {
-			return i, KittyKeyboardMsg(param)
+			return i, KeyboardEnhancementsMsg{kittyFlags: param}
 		}
 	case 'R' | '?'<<parser.MarkerShift:
 		// This report may return a third parameter representing the page
@@ -266,7 +266,7 @@ func parseCsi(b []byte) (int, Msg) {
 	case 'm' | '>'<<parser.MarkerShift:
 		// XTerm modifyOtherKeys
 		if paramsLen == 2 && csi.Param(0) == 4 && csi.Param(1) != -1 {
-			return i, ModifyOtherKeysMsg(csi.Param(1))
+			return i, KeyboardEnhancementsMsg{modifyOtherKeys: csi.Param(1)}
 		}
 	case 'I':
 		return i, FocusMsg{}

screen_test.go
@@ -74,8 +74,8 @@ func TestClearMsg(t *testing.T) {
 		},
 		{
 			name:     "kitty_start",
-			cmds:     []Cmd{DisableKittyKeyboard, EnableKittyKeyboard(3)},
-			expected: "\x1b[?25l\x1b[?2004h\x1b[?2027h\x1b[?2027$p\x1b[>u\x1b[>3u\rsuccess\r\n\x1b[D\x1b[2K\r\x1b[?2004l\x1b[?25h\x1b[>0u",
+			cmds:     []Cmd{DisableKeyboardEnhancements, EnableKeyboardEnhancements(WithKeyReleases)},
+			expected: "\x1b[?25l\x1b[?2004h\x1b[?2027h\x1b[?2027$p\x1b[>4;1m\x1b[>3u\rsuccess\r\n\x1b[D\x1b[2K\r\x1b[?2004l\x1b[?25h\x1b[>4;0m\x1b[>0u",
 		},
 	}
 

commit a039baa8732650f3561227dd6645853cc880c7e4

fix: properly store kitty keyboard flags
Fixes: 7cb34becafd6 ((v2) feat: combine keyboard enhancements into a nicer API (#1152))

tea.go
@@ -468,11 +468,11 @@ func (p *Program) eventLoop(model Model, cmds chan Cmd) (Model, error) {
 				p.execute(ansi.RequestCursorColor)
 
 			case KeyboardEnhancementsMsg:
-				if msg.kittyFlags != p.keyboard.kittyFlags {
-					msg.kittyFlags |= p.keyboard.kittyFlags
+				if p.keyboard.kittyFlags != msg.kittyFlags {
+					p.keyboard.kittyFlags |= msg.kittyFlags
 				}
-				if msg.modifyOtherKeys == 0 {
-					msg.modifyOtherKeys = p.keyboard.modifyOtherKeys
+				if p.keyboard.modifyOtherKeys == 0 || msg.modifyOtherKeys > p.keyboard.modifyOtherKeys {
+					p.keyboard.modifyOtherKeys = msg.modifyOtherKeys
 				}
 
 			case enableKeyboardEnhancementsMsg: