charmbracelet/bubbletea

commit c53a7c99d2e5a2ea935612a528a834f8863fcefc

fix: kitty: use the correct case for key text

kitty.go
@@ -253,12 +253,16 @@ func parseKittyKeyboard(csi *ansi.CsiSequence) (msg Msg) {
 		}
 	}
 
-	noneOrShifted := key.Mod <= ModShift && key.Code != KeyLeftShift && key.Code != KeyRightShift
-	if len(key.Text) == 0 && noneOrShifted {
-		if key.ShiftedCode != 0 && unicode.IsPrint(key.ShiftedCode) {
+	if len(key.Text) == 0 && unicode.IsPrint(key.Code) &&
+		(key.Mod <= ModShift || key.Mod == ModCapsLock) {
+		desiredCase := unicode.ToLower
+		if key.Mod == ModShift || key.Mod == ModCapsLock {
+			desiredCase = unicode.ToUpper
+		}
+		if key.ShiftedCode != 0 {
 			key.Text = string(key.ShiftedCode)
-		} else if unicode.IsPrint(key.Code) {
-			key.Text = string(key.Code)
+		} else {
+			key.Text = string(desiredCase(key.Code))
 		}
 	}
 

commit 8a91939f5c61a327bfa4ffa671d8e14c13b6259f

fix: kitty: correctly handle printable characters with no modifiers
When a printable character is pressed with no modifiers, the `key.Mod`
field is set to 0. In this case, the `key.Code` field should be used to
determine the text to send to the program.

Fixes: c53a7c99d2e5 (fix: kitty: use the correct case for key text)

kitty.go
@@ -271,14 +271,18 @@ func parseKittyKeyboard(csi *ansi.CsiSequence) (msg Msg) {
 
 	if len(key.Text) == 0 && unicode.IsPrint(key.Code) &&
 		(key.Mod <= ModShift || key.Mod == ModCapsLock) {
-		desiredCase := unicode.ToLower
-		if key.Mod == ModShift || key.Mod == ModCapsLock {
-			desiredCase = unicode.ToUpper
-		}
-		if key.ShiftedCode != 0 {
-			key.Text = string(key.ShiftedCode)
+		if key.Mod == 0 {
+			key.Text = string(key.Code)
 		} else {
-			key.Text = string(desiredCase(key.Code))
+			desiredCase := unicode.ToLower
+			if key.Mod == ModShift || key.Mod == ModCapsLock {
+				desiredCase = unicode.ToUpper
+			}
+			if key.ShiftedCode != 0 {
+				key.Text = string(key.ShiftedCode)
+			} else {
+				key.Text = string(desiredCase(key.Code))
+			}
 		}
 	}
 