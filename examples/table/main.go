690a0e685721b28800369d2c32346b7798eb8dfe
bashbunni <bunni@bashbunni.dev>
2024-10-16 20:56:44 -0700

    feat(example): set table height based on window height

--- examples/table/main.go
+++ examples/table/main.go
@@ -22,6 +22,8 @@ func (m model) Init() tea.Cmd { return nil }
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.table.SetHeight(msg.Height - 1)
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "esc":
