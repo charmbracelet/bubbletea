a3266cac54a76ea0c5b52cf2c85ae7fe08c45908
Dolev Hadar <dolev@komodor.io>
2024-10-17 20:46:27 +0300

    feat(examples): tree

--- examples/tree/main.go
+++ examples/tree/main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/tree"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/x/ansi"
 )
 
 type model struct {
-	tree tree.Model
+	tree   tree.Model
+	choice *tree.Item
 }
 
 func (m model) Init() tea.Cmd {
@@ -21,6 +25,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyPressMsg:
 		switch msg.String() {
+		case "e":
+			m.choice = m.tree.ItemAtCurrentOffset()
+			return m, tea.Quit
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		}
@@ -33,17 +40,73 @@ func (m model) View() string {
 	return m.tree.View()
 }
 
+type file struct {
+	name  string
+	color string
+}
+
+func (f file) String() string {
+	// TODO: can't partially apply the foreground only the icon
+	// This happens because creating the new style somehow resets the background of the selected node
+	return "⌯ " + lipgloss.NewStyle().Foreground(lipgloss.Color(f.color)).Render(f.name)
+}
+
+type dir struct {
+	name string
+}
+
+func (d dir) String() string {
+	// TODO: can't partially apply the foreground only the icon
+	// This happens because creating the new style somehow resets the background of the selected node
+	return lipgloss.NewStyle().Foreground(lipgloss.Color("4")).Render(d.name)
+}
+
 func main() {
 	t := tree.New(
-		tree.Root("root").Child("child1").Child("child2").Child(
-			tree.Root("child3").Child("child4"),
-		),
+		tree.Root(dir{"charmbracelet/lipgloss"}).
+			Child(
+				tree.Root(dir{"tree"}).
+					Child(file{"tree.go", "6"}).
+					Child(file{"renderer.go", "6"}),
+			).
+			Child(
+				tree.Root(dir{"table"}).
+					Child(
+						tree.Root(dir{"utils"}).
+							Child(file{"utils.go", "6"}),
+					),
+			).
+			Child(tree.Root(dir{"list"})).
+			Child(file{"README.md", "3"}).
+			Child(file{"go.mod", "255"}).
+			Child(file{"go.sum", "255"}).
+			Child(file{".gitignore", "255"}),
+		80,
+		30,
 	)
-	m := model{tree: t}
+	t.OpenCharacter = "📂"
+	t.ClosedCharacter = "📁"
+	t.Styles.SelectedNode = lipgloss.NewStyle().Background(lipgloss.Color("8"))
+	kb := []key.Binding{
+		key.NewBinding(key.WithKeys("e"), key.WithHelp("e", "select")),
+	}
+	t.AdditionalShortHelpKeys = func() []key.Binding {
+		return kb
+	}
+	t.AdditionalFullHelpKeys = func() []key.Binding {
+		return kb
+	}
 
-	if _, err := tea.NewProgram(m).Run(); err != nil {
-		fmt.Println("Uh oh, we encountered an error:", err)
+	p := tea.NewProgram(model{tree: t})
+	m, err := p.Run()
+	if err != nil {
+		fmt.Println("Oh no:", err)
 		os.Exit(1)
 	}
-	fmt.Println(t.View())
+
+	// Assert the final tea.Model to our local model and print the choice.
+	if m, ok := m.(model); ok && m.choice != nil {
+		// TODO: should we expose a OriginalValue() method on Item?
+		fmt.Printf("---\nYou chose %s!\n", ansi.Strip(m.choice.Value()))
+	}
 }
