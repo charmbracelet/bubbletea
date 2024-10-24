a469bc89227358243dca22e19c8c4277fa08979c
Christian Rocha <christian@rocha.is>
2024-10-24 17:11:28 -0400

    docs(examples): add context to cursor example

--- examples/cursor-style/main.go
+++ examples/cursor-style/main.go
@@ -13,7 +13,7 @@ type model struct {
 }
 
 func (m model) Init() (tea.Model, tea.Cmd) {
-	return m, tea.ShowCursor
+	return m, tea.Batch(tea.ShowCursor, tea.SetCursorStyle(m.style, m.steady))
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -23,7 +23,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+q", "q":
 			return m, tea.Quit
-		case "left":
+		case "h", "left":
 			if m.style == tea.CursorBlock && !m.steady {
 				break
 			}
@@ -32,7 +32,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 			m.steady = !m.steady
 			cmd = tea.SetCursorStyle(m.style, m.steady)
-		case "right":
+		case "l", "right":
 			if m.style == tea.CursorBar && m.steady {
 				break
 			}
@@ -49,7 +49,30 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	return "Press left/right to change the cursor style, q or ctrl+c to quit." +
 		"\n\n" +
-		"  <- This is a cursor"
+		"  <- This is the cursor (a " + m.describeCursor() + ")"
+}
+
+func (m model) describeCursor() string {
+	var (
+		adj, noun string
+	)
+
+	if m.steady {
+		adj = "steady"
+	} else {
+		adj = "blinking"
+	}
+
+	switch m.style {
+	case tea.CursorBlock:
+		noun = "block"
+	case tea.CursorUnderline:
+		noun = "underline"
+	case tea.CursorBar:
+		noun = "bar"
+	}
+
+	return fmt.Sprintf("%s %s", adj, noun)
 }
 
 func main() {
