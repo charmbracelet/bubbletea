charmbracelet/bubbletea

commit 83a84e4593b7b21fff146477bf99f3a9140e95d2

refactor: move input tracing to input parser and simplify tracing
This commit wraps the input parser in a struct and moves the tracing
logic to the program and its writer and input driver.

It also changes the behavior of the tracer to only enable tracing for
local environment variables i.e. from `os.Getenv` and not from the
program's environment. In other words, it will ignore the program's
environment variables when it comes to tracing.

parse.go
@@ -85,14 +85,16 @@ const (
 	_FlagFKeys
 )
 
-var flags int
+// inputParser is a parser for input escape sequences.
+// TODO: Use [ansi.DecodeSequence] instead of this parser.
+type inputParser struct {
+	flags int
+}
 
 // setFlags sets the flags for the parser.
 // This will control the behavior of ParseSequence.
-//
-//nolint:unused
-func setFlags(f int) {
-	flags = f
+func (p *inputParser) setFlags(f int) {
+	p.flags = f
 }
 
 // parseSequence finds the first recognized event sequence and returns it along
@@ -100,7 +102,7 @@ func setFlags(f int) {
 //
 // It will return zero and nil no sequence is recognized or when the buffer is
 // empty. If a sequence is not supported, an UnknownEvent is returned.
-func parseSequence(buf []byte) (n int, msg Msg) {
+func (p *inputParser) parseSequence(buf []byte) (n int, msg Msg) {
 	if len(buf) == 0 {
 		return 0, nil
 	}
@@ -114,17 +116,17 @@ func parseSequence(buf []byte) (n int, msg Msg) {
 
 		switch b := buf[1]; b {
 		case 'O': // Esc-prefixed SS3
-			return parseSs3(buf)
+			return p.parseSs3(buf)
 		case 'P': // Esc-prefixed DCS
-			return parseDcs(buf)
+			return p.parseDcs(buf)
 		case '[': // Esc-prefixed CSI
-			return parseCsi(buf)
+			return p.parseCsi(buf)
 		case ']': // Esc-prefixed OSC
-			return parseOsc(buf)
+			return p.parseOsc(buf)
 		case '_': // Esc-prefixed APC
-			return parseApc(buf)
+			return p.parseApc(buf)
 		default:
-			n, e := parseSequence(buf[1:])
+			n, e := p.parseSequence(buf[1:])
 			if k, ok := e.(KeyPressMsg); ok {
 				k.Text = ""
 				k.Mod |= ModAlt
@@ -136,18 +138,18 @@ func parseSequence(buf []byte) (n int, msg Msg) {
 			return 1, KeyPressMsg{Code: KeyEscape}
 		}
 	case ansi.SS3:
-		return parseSs3(buf)
+		return p.parseSs3(buf)
 	case ansi.DCS:
-		return parseDcs(buf)
+		return p.parseDcs(buf)
 	case ansi.CSI:
-		return parseCsi(buf)
+		return p.parseCsi(buf)
 	case ansi.OSC:
-		return parseOsc(buf)
+		return p.parseOsc(buf)
 	case ansi.APC:
-		return parseApc(buf)
+		return p.parseApc(buf)
 	default:
 		if b <= ansi.US || b == ansi.DEL || b == ansi.SP {
-			return 1, parseControl(b)
+			return 1, p.parseControl(b)
 		} else if b >= ansi.PAD && b <= ansi.APC {
 			// C1 control code
 			// UTF-8 never starts with a C1 control code
@@ -155,11 +157,11 @@ func parseSequence(buf []byte) (n int, msg Msg) {
 			code := rune(b) - 0x40
 			return 1, KeyPressMsg{Code: code, Mod: ModCtrl | ModAlt}
 		}
-		return parseUtf8(buf)
+		return p.parseUtf8(buf)
 	}
 }
 
-func parseCsi(b []byte) (int, Msg) {
+func (p *inputParser) parseCsi(b []byte) (int, Msg) {
 	if len(b) == 2 && b[0] == ansi.ESC {
 		// short cut if this is an alt+[ key
 		return 2, KeyPressMsg{Text: string(rune(b[1])), Mod: ModAlt}
@@ -224,7 +226,7 @@ func parseCsi(b []byte) (int, Msg) {
 		// CSI <number> $ is an invalid sequence, but URxvt uses it for
 		// shift modified keys.
 		if b[i-1] == '$' {
-			n, ev := parseCsi(append(b[:i-1], '~'))
+			n, ev := p.parseCsi(append(b[:i-1], '~'))
 			if k, ok := ev.(KeyPressMsg); ok {
 				k.Mod |= ModShift
 				return n, k
@@ -396,7 +398,7 @@ func parseCsi(b []byte) (int, Msg) {
 			var k KeyPressMsg
 			switch param {
 			case 1:
-				if flags&_FlagFind != 0 {
+				if p.flags&_FlagFind != 0 {
 					k = KeyPressMsg{Code: KeyFind}
 				} else {
 					k = KeyPressMsg{Code: KeyHome}
@@ -406,7 +408,7 @@ func parseCsi(b []byte) (int, Msg) {
 			case 3:
 				k = KeyPressMsg{Code: KeyDelete}
 			case 4:
-				if flags&_FlagSelect != 0 {
+				if p.flags&_FlagSelect != 0 {
 					k = KeyPressMsg{Code: KeySelect}
 				} else {
 					k = KeyPressMsg{Code: KeyEnd}
@@ -455,7 +457,7 @@ func parseCsi(b []byte) (int, Msg) {
 
 // parseSs3 parses a SS3 sequence.
 // See https://vt100.net/docs/vt220-rm/chapter4.html#S4.4.4.2
-func parseSs3(b []byte) (int, Msg) {
+func (p *inputParser) parseSs3(b []byte) (int, Msg) {
 	if len(b) == 2 && b[0] == ansi.ESC {
 		// short cut if this is an alt+O key
 		return 2, KeyPressMsg{Code: rune(b[1]), Mod: ModAlt}
@@ -519,7 +521,7 @@ func parseSs3(b []byte) (int, Msg) {
 	return i, k
 }
 
-func parseOsc(b []byte) (int, Msg) {
+func (p *inputParser) parseOsc(b []byte) (int, Msg) {
 	if len(b) == 2 && b[0] == ansi.ESC {
 		// short cut if this is an alt+] key
 		return 2, KeyPressMsg{Code: rune(b[1]), Mod: ModAlt}
@@ -613,7 +615,7 @@ func parseOsc(b []byte) (int, Msg) {
 }
 
 // parseStTerminated parses a control sequence that gets terminated by a ST character.
-func parseStTerminated(intro8, intro7 byte) func([]byte) (int, Msg) {
+func (p *inputParser) parseStTerminated(intro8, intro7 byte) func([]byte) (int, Msg) {
 	return func(b []byte) (int, Msg) {
 		var i int
 		if b[i] == intro8 || b[i] == ansi.ESC {
@@ -644,7 +646,7 @@ func parseStTerminated(intro8, intro7 byte) func([]byte) (int, Msg) {
 	}
 }
 
-func parseDcs(b []byte) (int, Msg) {
+func (p *inputParser) parseDcs(b []byte) (int, Msg) {
 	if len(b) == 2 && b[0] == ansi.ESC {
 		// short cut if this is an alt+P key
 		return 2, KeyPressMsg{Code: rune(b[1]), Mod: ModAlt}
@@ -757,17 +759,17 @@ func parseDcs(b []byte) (int, Msg) {
 	return i, UnknownMsg(b[:i])
 }
 
-func parseApc(b []byte) (int, Msg) {
+func (p *inputParser) parseApc(b []byte) (int, Msg) {
 	if len(b) == 2 && b[0] == ansi.ESC {
 		// short cut if this is an alt+_ key
 		return 2, KeyPressMsg{Code: rune(b[1]), Mod: ModAlt}
 	}
 
 	// APC sequences are introduced by APC (0x9f) or ESC _ (0x1b 0x5f)
-	return parseStTerminated(ansi.APC, '_')(b)
+	return p.parseStTerminated(ansi.APC, '_')(b)
 }
 
-func parseUtf8(b []byte) (int, Msg) {
+func (p *inputParser) parseUtf8(b []byte) (int, Msg) {
 	if len(b) == 0 {
 		return 0, nil
 	}
@@ -775,7 +777,7 @@ func parseUtf8(b []byte) (int, Msg) {
 	c := b[0]
 	if c <= ansi.US || c == ansi.DEL || c == ansi.SP {
 		// Control codes get handled by parseControl
-		return 1, parseControl(c)
+		return 1, p.parseControl(c)
 	} else if c > ansi.US && c < ansi.DEL {
 		// ASCII printable characters
 		code := rune(c)
@@ -808,32 +810,32 @@ func parseUtf8(b []byte) (int, Msg) {
 	return len(cluster), KeyPressMsg{Code: code, Text: text}
 }
 
-func parseControl(b byte) Msg {
+func (p *inputParser) parseControl(b byte) Msg {
 	switch b {
 	case ansi.NUL:
-		if flags&_FlagCtrlAt != 0 {
+		if p.flags&_FlagCtrlAt != 0 {
 			return KeyPressMsg{Code: '@', Mod: ModCtrl}
 		}
 		return KeyPressMsg{Code: KeySpace, Mod: ModCtrl}
 	case ansi.BS:
 		return KeyPressMsg{Code: 'h', Mod: ModCtrl}
 	case ansi.HT:
-		if flags&_FlagCtrlI != 0 {
+		if p.flags&_FlagCtrlI != 0 {
 			return KeyPressMsg{Code: 'i', Mod: ModCtrl}
 		}
 		return KeyPressMsg{Code: KeyTab}
 	case ansi.CR:
-		if flags&_FlagCtrlM != 0 {
+		if p.flags&_FlagCtrlM != 0 {
 			return KeyPressMsg{Code: 'm', Mod: ModCtrl}
 		}
 		return KeyPressMsg{Code: KeyEnter}
 	case ansi.ESC:
-		if flags&_FlagCtrlOpenBracket != 0 {
+		if p.flags&_FlagCtrlOpenBracket != 0 {
 			return KeyPressMsg{Code: '[', Mod: ModCtrl}
 		}
 		return KeyPressMsg{Code: KeyEscape}
 	case ansi.DEL:
-		if flags&_FlagBackspace != 0 {
+		if p.flags&_FlagBackspace != 0 {
 			return KeyPressMsg{Code: KeyDelete}
 		}
 		return KeyPressMsg{Code: KeyBackspace}

driver.go
@@ -3,6 +3,7 @@ package tea
 import (
 	"bytes"
 	"io"
+	"log"
 	"unicode/utf8"
 
 	"github.com/muesli/cancelreader"
@@ -44,7 +45,8 @@ type driver struct {
 	// It is used to decode ANSI escape sequences and utf16 sequences.
 	keyState win32InputState //nolint:unused
 
-	flags int // control the behavior of the driver.
+	parser inputParser
+	trace  bool // trace enables input tracing and logging.
 }
 
 // newDriver returns a new ANSI input driver.
@@ -61,7 +63,7 @@ func newDriver(r io.Reader, term string, flags int) (*driver, error) {
 	d.rd = cr
 	d.table = buildKeysTable(flags, term)
 	d.term = term
-	d.flags = flags
+	d.parser.flags = flags
 	return d, nil
 }
 
@@ -93,7 +95,10 @@ func (d *driver) readEvents() (msgs []Msg, err error) {
 
 	var i int
 	for i < len(buf) {
-		nb, ev := parseSequence(buf[i:])
+		nb, ev := d.parser.parseSequence(buf[i:])
+		if d.trace {
+			log.Printf("input: %q", buf[i:i+nb])
+		}
 
 		// Handle bracketed-paste
 		if d.paste != nil {

focus_test.go
@@ -5,7 +5,8 @@ import (
 )
 
 func TestFocus(t *testing.T) {
-	_, e := parseSequence([]byte("\x1b[I"))
+	var p inputParser
+	_, e := p.parseSequence([]byte("\x1b[I"))
 	switch e.(type) {
 	case FocusMsg:
 		// ok
@@ -15,7 +16,8 @@ func TestFocus(t *testing.T) {
 }
 
 func TestBlur(t *testing.T) {
-	_, e := parseSequence([]byte("\x1b[O"))
+	var p inputParser
+	_, e := p.parseSequence([]byte("\x1b[O"))
 	switch e.(type) {
 	case BlurMsg:
 		// ok

key_test.go
@@ -323,12 +323,13 @@ func TestParseSequence(t *testing.T) {
 		})
 	}
 
+	var p inputParser
 	for _, tc := range td {
 		t.Run(fmt.Sprintf("%q", string(tc.seq)), func(t *testing.T) {
 			var events []Msg
 			buf := tc.seq
 			for len(buf) > 0 {
-				width, msg := parseSequence(buf)
+				width, msg := p.parseSequence(buf)
 				switch msg := msg.(type) {
 				case multiMsg:
 					events = append(events, msg...)
@@ -776,6 +777,7 @@ func genRandomDataWithSeed(s int64, length int) randTest {
 }
 
 func FuzzParseSequence(f *testing.F) {
+	var p inputParser
 	for seq := range sequences {
 		f.Add(seq)
 	}
@@ -784,7 +786,7 @@ func FuzzParseSequence(f *testing.F) {
 	f.Add("\x1bP>|charm terminal(0.1.2)\x1b\\") // DCS (XTVERSION)
 	f.Add("\x1b_Gi=123\x1b\\")                  // APC
 	f.Fuzz(func(t *testing.T, seq string) {
-		n, _ := parseSequence([]byte(seq))
+		n, _ := p.parseSequence([]byte(seq))
 		if n == 0 && seq != "" {
 			t.Errorf("expected a non-zero width for %q", seq)
 		}
@@ -794,10 +796,11 @@ func FuzzParseSequence(f *testing.F) {
 // BenchmarkDetectSequenceMap benchmarks the map-based sequence
 // detector.
 func BenchmarkDetectSequenceMap(b *testing.B) {
+	var p inputParser
 	td := genRandomDataWithSeed(123, 10000)
 	for i := 0; i < b.N; i++ {
 		for j, w := 0, 0; j < len(td.data); j += w {
-			w, _ = parseSequence(td.data[j:])
+			w, _ = p.parseSequence(td.data[j:])
 		}
 	}
 }

parse_test.go
@@ -19,11 +19,13 @@ func TestParseSequence_Events(t *testing.T) {
 		KeyPressMsg{Code: KeyEscape, Mod: ModShift},
 		modeReportMsg{Mode: 1049, Value: 2},
 	}
+
+	var p inputParser
 	for i := 0; len(input) != 0; i++ {
 		if i >= len(want) {
 			t.Fatalf("reached end of want events")
 		}
-		n, got := parseSequence(input)
+		n, got := p.parseSequence(input)
 		if !reflect.DeepEqual(got, want[i]) {
 			t.Errorf("got %#v (%T), want %#v (%T)", got, got, want[i], want[i])
 		}
@@ -32,10 +34,11 @@ func TestParseSequence_Events(t *testing.T) {
 }
 
 func BenchmarkParseSequence(b *testing.B) {
+	var p inputParser
 	input := []byte("\x1b\x1b[Ztest\x00\x1b]10;1234/1234/1234\x07\x1b[27;2;27~")
 	b.ReportAllocs()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		parseSequence(input)
+		p.parseSequence(input)
 	}
 }

tea.go
@@ -15,7 +15,6 @@ import (
 	"fmt"
 	"image/color"
 	"io"
-	"log"
 	"os"
 	"os/signal"
 	"runtime"
@@ -190,6 +189,7 @@ type Program struct {
 	ttyInput              term.File
 	previousTtyInputState *term.State
 	inputReader           *driver
+	traceInput            bool // true if input should be traced
 	readLoopDone          chan struct{}
 
 	// modes keeps track of terminal modes that have been enabled or disabled.
@@ -217,9 +217,6 @@ type Program struct {
 	// is paused. This saves the terminal colors state so they can be restored
 	// when the program is resumed.
 	setBg, setFg, setCc color.Color
-
-	// tracer is used to trace the program's output and input.
-	tracer tracer
 }
 
 // Quit is a special command that tells the Bubble Tea program to exit.
@@ -286,27 +283,21 @@ func NewProgram(model Model, opts ...ProgramOption) *Program {
 	}
 
 	// Detect if tracing is enabled.
-	if tracePath := p.getenv("TEA_TRACE"); tracePath != "" {
+	if tracePath := os.Getenv("TEA_TRACE"); tracePath != "" {
 		switch tracePath {
 		case "0", "false", "off":
 			break
 		}
 
-		tracer, err := newTracer(tracePath)
-		if err == nil {
-			p.tracer = tracer
+		if _, err := LogToFile(tracePath, "bubbletea"); err == nil {
 			// Enable different types of tracing.
-			if output, _ := strconv.ParseBool(p.getenv("TEA_TRACE_OUTPUT")); output {
-				p.tracer = p.tracer.withOutput()
+			if output, _ := strconv.ParseBool(os.Getenv("TEA_TRACE_OUTPUT")); output {
+				p.output.trace = true
 			}
-			if input, _ := strconv.ParseBool(p.getenv("TEA_TRACE_INPUT")); input {
-				p.tracer = p.tracer.withInput()
+			if input, _ := strconv.ParseBool(os.Getenv("TEA_TRACE_INPUT")); input {
+				p.traceInput = true
 			}
 		}
-	}
-
-	if p.tracer.mask&traceOutput != 0 {
-		p.output.trace = true
 	}
 
 	return p
@@ -419,12 +410,6 @@ func (p *Program) eventLoop(model Model, cmds chan Cmd) (Model, error) {
 			}
 			if msg == nil {
 				continue
-			}
-
-			// XXX: Is this the right place to trace input? Perhaps inside
-			// [parseSequence]?
-			if p.tracer.mask&traceInput != 0 {
-				log.Printf("input %T %v", msg, msg)
 			}
 
 			// Handle special internal messages.

trace.go
@@ -1,50 +1,0 @@
-package tea
-
-import "os"
-
-// tracerMask is a bitmask that represents the different types of tracing that
-// can be enabled.
-type tracerMask int
-
-// Constants that represent the different types of tracing that can be enabled.
-const (
-	traceOutput tracerMask = 1 << iota
-	traceInput
-)
-
-// tracer is a type that can be used to trace ANSI sequences and I/O of a
-// program.
-type tracer struct {
-	file *os.File
-	op   string
-	mask tracerMask
-}
-
-// newTracer returns a new tracer for the given path and mask options.
-func newTracer(fp string, masks ...tracerMask) (tracer, error) {
-	var mask tracerMask
-	for _, m := range masks {
-		mask |= m
-	}
-
-	t := tracer{op: fp, mask: mask}
-	f, err := LogToFile(fp, "bubbletea")
-	if err != nil {
-		return t, err
-	}
-
-	t.file = f
-	return t, nil
-}
-
-// withOutput returns a new tracer that traces output.
-func (t tracer) withOutput() tracer {
-	t.mask |= traceOutput
-	return t
-}
-
-// withInput returns a new tracer that traces input.
-func (t tracer) withInput() tracer {
-	t.mask |= traceInput
-	return t
-}

tty.go
@@ -134,6 +134,7 @@ func (p *Program) initInputReader() error {
 		return err
 	}
 
+	drv.trace = p.traceInput
 	p.inputReader = drv
 	p.readLoopDone = make(chan struct{})
 	go p.readLoop()

commit 13a18ac631dc7cadc02970e206f012c113fcceb7

fix: refactor: move input tracing to input parser and simplify tracing
Fixes: 83a84e4593b7 (refactor: move input tracing to input parser and simplify tracing)

driver_other.go
@@ -12,6 +12,6 @@ func (d *driver) ReadEvents() ([]Msg, error) {
 
 // parseWin32InputKeyEvent parses a Win32 input key events. This function is
 // only available on Windows.
-func parseWin32InputKeyEvent(*win32InputState, uint16, uint16, rune, bool, uint32, uint16) Msg {
+func (p *inputParser) parseWin32InputKeyEvent(*win32InputState, uint16, uint16, rune, bool, uint32, uint16) Msg {
 	return nil
 }

driver_windows.go
@@ -47,7 +47,7 @@ func (d *driver) handleConInput(
 
 	var evs []Msg
 	for _, event := range events {
-		if e := parseConInputEvent(event, &d.keyState); e != nil {
+		if e := d.parseConInputEvent(event, &d.keyState); e != nil {
 			if multi, ok := e.(multiMsg); ok {
 				evs = append(evs, multi...)
 			} else {
@@ -59,11 +59,11 @@ func (d *driver) handleConInput(
 	return evs, nil
 }
 
-func parseConInputEvent(event xwindows.InputRecord, keyState *win32InputState) Msg {
+func (d *driver) parseConInputEvent(event xwindows.InputRecord, keyState *win32InputState) Msg {
 	switch event.EventType {
 	case xwindows.KEY_EVENT:
 		kevent := event.KeyEvent()
-		return parseWin32InputKeyEvent(keyState, kevent.VirtualKeyCode, kevent.VirtualScanCode,
+		return d.parser.parseWin32InputKeyEvent(keyState, kevent.VirtualKeyCode, kevent.VirtualScanCode,
 			kevent.Char, kevent.KeyDown, kevent.ControlKeyState, kevent.RepeatCount)
 
 	case xwindows.WINDOW_BUFFER_SIZE_EVENT:
@@ -212,7 +212,7 @@ func peekConsoleInput(console windows.Handle, inputRecords []xwindows.InputRecor
 // an event from win32-input-mode. Otherwise, it's a key event from the Windows
 // Console API and needs a state to decode ANSI escape sequences and utf16
 // runes.
-func parseWin32InputKeyEvent(state *win32InputState, vkc uint16, _ uint16, r rune, keyDown bool, cks uint32, repeatCount uint16) (msg Msg) {
+func (p *inputParser) parseWin32InputKeyEvent(state *win32InputState, vkc uint16, _ uint16, r rune, keyDown bool, cks uint32, repeatCount uint16) (msg Msg) {
 	defer func() {
 		// Respect the repeat count.
 		if repeatCount > 1 {
@@ -268,7 +268,7 @@ func parseWin32InputKeyEvent(state *win32InputState, vkc uint16, _ uint16, r run
 				return nil
 			}
 
-			n, msg := parseSequence(state.ansiBuf[:state.ansiIdx])
+			n, msg := p.parseSequence(state.ansiBuf[:state.ansiIdx])
 			if n == 0 {
 				return nil
 			}

parse.go
@@ -350,7 +350,7 @@ func (p *inputParser) parseCsi(b []byte) (int, Msg) {
 			rc = 1
 		}
 
-		event := parseWin32InputKeyEvent(
+		event := p.parseWin32InputKeyEvent(
 			nil,
 			uint16(csi.Param(0)), //nolint:gosec // Vk wVirtualKeyCode
 			uint16(csi.Param(1)), //nolint:gosec // Sc wVirtualScanCode