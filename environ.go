57f505c01b985801916bd479e0d220ad818332d3
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-01-16 17:24:18 +0300

    docs: add examples and godox to EnvMsg and Raw

--- environ.go
+++ environ.go
@@ -41,7 +41,19 @@ func (p environ) LookupEnv(key string) (s string, v bool) {
 }
 
 // EnvMsg is a message that represents the environment variables of the
-// program. This message is sent to the program when it starts.
+// program. This is useful for getting the environment variables of programs
+// running in a remote session like SSH. In that case, using [os.Getenv] would
+// return the server's environment variables, not the client's.
+//
+// This message is sent to the program when it starts.
+//
+// Example:
+//
+//	switch msg := msg.(type) {
+//	case EnvMsg:
+//	  // What terminal type is being used?
+//	  term := msg.Getenv("TERM")
+//	}
 type EnvMsg environ
 
 // Getenv returns the value of the environment variable named by the key. If
