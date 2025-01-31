acbccd0987e9fb17e27b23cbb2047caf5386796f
Christian Rocha <christian@rocha.is>
2025-01-31 16:30:15 -0500

    docs(examples): bump bubbles@v2-exp

--- examples/textarea/main.go
+++ examples/textarea/main.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/charmbracelet/bubbles/v2/textarea"
 	tea "github.com/charmbracelet/bubbletea/v2"
-	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/v2"
 )
 
 func main() {
