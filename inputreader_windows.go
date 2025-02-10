charmbracelet/bubbletea

commit c8d6005325d3b370ea48d3e0c2129792d988e471

fix: use CancelIoEx to cancel Windows conInputReader across threads (#1305)
The Windows conInputReader uses `windows.CancelIo` to close the read
loop's input handle. Windows `CancelIo` docs indicate that call is only
appropriate for I/O operations on the same thread as the `CancelIo`
call. Since the read loop is in its own goroutine, it's likely to be on
another thread from the bubbletea Program.

This change adds a call to Windows `CancelIoEx`, which will cancel I/O
operations across threads.

Fixes #1167

inputreader_windows.go
@@ -57,7 +57,7 @@ func newInputReader(r io.Reader) (cancelreader.CancelReader, error) {
 func (r *conInputReader) Cancel() bool {
 	r.setCanceled()
 
-	return windows.CancelIo(r.conin) == nil
+	return windows.CancelIoEx(r.conin, nil) == nil || windows.CancelIo(r.conin) == nil
 }
 
 // Close implements cancelreader.CancelReader.

commit b639a0a686cf28c92cf065b3f7ecc97291e066bf

fix: windows: handle cancel io error
This gracefully handles windows.CancelIo errors.

Fixes: c8d6005325d3 (fix: use CancelIoEx to cancel Windows conInputReader across threads (#1305))
Fixes: https://github.com/charmbracelet/bubbletea/issues/1324

inputreader_windows.go
@@ -73,8 +73,11 @@ func (r *conInputReader) Close() error {
 }
 
 // Read implements cancelreader.CancelReader.
-func (*conInputReader) Read(_ []byte) (n int, err error) {
-	return 0, nil
+func (r *conInputReader) Read(_ []byte) (n int, err error) {
+	if r.isCanceled() {
+		err = cancelreader.ErrCanceled
+	}
+	return
 }
 
 func prepareConsole(input windows.Handle, modes ...uint32) (originalMode uint32, err error) {
@@ -104,4 +107,11 @@ func (c *cancelMixin) setCanceled() {
 	defer c.lock.Unlock()
 
 	c.unsafeCanceled = true
+}
+
+func (c *cancelMixin) isCanceled() bool {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	return c.unsafeCanceled
 }

key_windows.go
@@ -10,23 +10,26 @@ import (
 
 	"github.com/erikgeiser/coninput"
 	localereader "github.com/mattn/go-localereader"
-	"golang.org/x/sys/windows"
+	"github.com/muesli/cancelreader"
 )
 
 func readInputs(ctx context.Context, msgs chan<- Msg, input io.Reader) error {
 	if coninReader, ok := input.(*conInputReader); ok {
-		return readConInputs(ctx, msgs, coninReader.conin)
+		return readConInputs(ctx, msgs, coninReader)
 	}
 
 	return readAnsiInputs(ctx, msgs, localereader.NewReader(input))
 }
 
-func readConInputs(ctx context.Context, msgsch chan<- Msg, con windows.Handle) error {
+func readConInputs(ctx context.Context, msgsch chan<- Msg, con *conInputReader) error {
 	var ps coninput.ButtonState                 // keep track of previous mouse state
 	var ws coninput.WindowBufferSizeEventRecord // keep track of the last window size event
 	for {
-		events, err := coninput.ReadNConsoleInputs(con, 16)
+		events, err := coninput.ReadNConsoleInputs(con.conin, 16)
 		if err != nil {
+			if con.isCanceled() {
+				return cancelreader.ErrCanceled
+			}
 			return fmt.Errorf("read coninput events: %w", err)
 		}
 