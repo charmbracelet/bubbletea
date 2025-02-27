15dd4fc90286f2680dda92d04b3428cd78409a8c
Andrey Nering <andreynering@users.noreply.github.com>
2025-02-27 11:49:38 -0300

    ci(lint): fix all lint issues on `main`

--- inputreader_other.go
+++ inputreader_other.go
@@ -4,11 +4,16 @@
 package tea
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/muesli/cancelreader"
 )
 
 func newInputReader(r io.Reader, _ bool) (cancelreader.CancelReader, error) {
-	return cancelreader.NewReader(r)
+	cr, err := cancelreader.NewReader(r)
+	if err != nil {
+		return nil, fmt.Errorf("bubbletea: error creating cancel reader: %w", err)
+	}
+	return cr, nil
 }
