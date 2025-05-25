f56921fcbc0b614bab67f848e4533330b5d305cf
Christian Rocha <christian@rocha.is>
2025-05-24 23:04:45 -0400

    feat: View type and Viewable interface

--- examples/textinput/main.go
+++ examples/textinput/main.go
@@ -65,13 +65,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
-func (m model) View() (string, *tea.Cursor) {
+func (m model) View() tea.View {
 	var c *tea.Cursor
 	if !m.textInput.VirtualCursor {
 		c = m.textInput.Cursor()
 		c.Y += lipgloss.Height(m.headerView())
 	}
-	return lipgloss.JoinVertical(lipgloss.Top, m.headerView(), m.textInput.View(), m.footerView()), c
+	str := lipgloss.JoinVertical(lipgloss.Top, m.headerView(), m.textInput.View(), m.footerView())
+
+	return tea.NewView(str).Cursor(c)
 }
 func (m model) headerView() string { return "What’s your favorite Pokémon?\n" }
 func (m model) footerView() string { return "\n(esc to quit)" }
