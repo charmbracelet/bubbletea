255445613b2e1578c75fd54df9a0b28606cf9c01
g.lenda <rminnich@me.com>
2025-06-30 18:39:36 +0000

    Finish plan 9 support. Depends on a pending commit to x/term. If that commit is pushed, then this go test works.

--- tty_plan9.go
+++ tty_plan9.go
@@ -2,25 +2,28 @@ package tea
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/charmbracelet/x/term"
 )
 
 func (p *Program) initInput() (err error) {
-	/*
-		// Check if input is a terminal
-		if f, ok := p.input.(term.File); ok && term.IsTerminal(f.Fd()) {
-			p.ttyInput = f
-			p.previousTtyInputState, err = term.MakeRaw(p.ttyInput.Fd())
-			if err != nil {
-				return fmt.Errorf("error entering raw mode: %w", err)
-			}
+	// Check if input is a terminal
+	if f, ok := p.input.(term.File); ok && term.IsTerminal(f.Fd()) {
+		p.ttyInput = f
+		p.previousTtyInputState, err = term.MakeRaw(p.ttyInput.Fd())
+		if err != nil {
+			return fmt.Errorf("error entering raw mode: %w", err)
 		}
+	}
 
-		if f, ok := p.output.(term.File); ok && term.IsTerminal(f.Fd()) {
-			p.ttyOutput = f
-		}
-	*/
-	return fmt.Errorf("no yet")
+	if f, ok := p.output.(term.File); ok && term.IsTerminal(f.Fd()) {
+		p.ttyOutput = f
+	}
+
+	return nil
 }
 
 func openInputTTY() (*os.File, error) {
@@ -35,11 +38,9 @@ const suspendSupported = false
 
 // Send SIGTSTP to the entire process group.
 func suspendProcess() {
-	/*
-	   c := make(chan os.Signal, 1)
-	   signal.Notify(c, syscall.SIGCONT)
-	   _ = syscall.Kill(0, syscall.SIGTSTP)
-	   // blocks until a CONT happens...
-	   <-c
-	*/
+	p := os.Getpid()
+	ctl := filepath.Join("proc", fmt.Sprintf("%d", p), "ctl")
+	if err := os.WriteFile(ctl, []byte("stop"), 0); err != nil {
+		log.Printf("Write sto to %q: %v", ctl, err)
+	}
 }
