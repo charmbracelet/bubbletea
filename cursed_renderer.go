charmbracelet/bubbletea

commit fdb86513b5e15c6d4a7886221aa2e452738c0e57

fix(renderer): always move cursor to bottom on close
Fixes: https://github.com/charmbracelet/crush/pull/1592

cursed_renderer.go
@@ -155,9 +155,13 @@ func (s *cursedRenderer) close() (err error) {
 
 		if lv.AltScreen {
 			enableAltScreen(s, false, true)
-		} else {
-			// Go to the bottom of the screen.
-			s.scr.MoveTo(0, s.cellbuf.Height()-1)
+		}
+		// Go to the bottom of the screen.
+		// We need to go to the bottom of the screen regardless of whether
+		// we're in alt screen mode or not to avoid leaving the cursor in the
+		// middle in terminals that don't support alt screen mode.
+		s.scr.MoveTo(0, s.cellbuf.Height()-1)
+		if !lv.AltScreen {
 			_, _ = s.scr.WriteString(ansi.EraseScreenBelow)
 		}
 		if lv.Cursor == nil {

commit 99c33bc3007094d114b7b5e412be03fae71728f8

fix(renderer): move cursor to bottom before disabling alt screen
Fixes: fdb86513b5e1 (fix(renderer): always move cursor to bottom on close)

cursed_renderer.go
@@ -153,15 +153,14 @@ func (s *cursedRenderer) close() (err error) {
 		// assuming the other screen is already reset when we switched screens.
 		_, _ = s.buf.WriteString(ansi.KittyKeyboard(0, 1))
 
-		if lv.AltScreen {
-			enableAltScreen(s, false, true)
-		}
 		// Go to the bottom of the screen.
 		// We need to go to the bottom of the screen regardless of whether
 		// we're in alt screen mode or not to avoid leaving the cursor in the
 		// middle in terminals that don't support alt screen mode.
 		s.scr.MoveTo(0, s.cellbuf.Height()-1)
-		if !lv.AltScreen {
+		if lv.AltScreen {
+			enableAltScreen(s, false, true)
+		} else {
 			_, _ = s.scr.WriteString(ansi.EraseScreenBelow)
 		}
 		if lv.Cursor == nil {