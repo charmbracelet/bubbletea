2e97c5b1e9422b362f0f51c6abbb253fe81c17e3
simpleqt <89645338+simpleqt@users.noreply.github.com>
2026-09-15 00:44:03 +0800

    fix: make Println/Printf no-op after Run returns
    Println and Printf sent directly on the unbuffered p.msgs channel
    without checking p.ctx.Done(). Any goroutine calling them after Run
    returned would block forever, leaking the goroutine. Route through
    a select with ctx.Done() so they become no-ops after shutdown, same
    as Send().

--- tea.go
+++ tea.go
@@ -1379,8 +1379,9 @@ func (p *Program) RestoreTerminal() error {
 //
 // If the altscreen is active no output will be printed.
 func (p *Program) Println(args ...any) {
-	p.msgs <- printLineMessage{
-		messageBody: fmt.Sprint(args...),
+	select {
+	case p.msgs <- printLineMessage{messageBody: fmt.Sprint(args...)}:
+	case <-p.ctx.Done():
 	}
 }
 
@@ -1393,8 +1394,9 @@ func (p *Program) Println(args ...any) {
 //
 // If the altscreen is active no output will be printed.
 func (p *Program) Printf(template string, args ...any) {
-	p.msgs <- printLineMessage{
-		messageBody: fmt.Sprintf(template, args...),
+	select {
+	case p.msgs <- printLineMessage{messageBody: fmt.Sprintf(template, args...)}:
+	case <-p.ctx.Done():
 	}
 }
 
